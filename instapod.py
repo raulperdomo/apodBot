38028a6a200ae5fff3dbcdefda269ae9eda67899
Raul Perdomo <raul@localhost.localdomain>
2018-05-17 08:29:37 -0400

    Added error handling and fixed formatting.

--- instapod.py
+++ instapod.py
@@ -6,7 +6,6 @@ import urllib
 from InstagramAPI import InstagramAPI
 
 apod = requests.get('http://apod.nasa.gov')
-print(apod.text)
 et = html.fromstring(apod.text)
 image = et.xpath('/html/body/center[1]/p[2]/a')
 imagePath = image.pop().attrib['href']
@@ -18,12 +17,14 @@ ex2 = et.xpath('/html/body/p[1]/text()')
 ex2.reverse()
 description = ''
 for child in explanation[0].iter():
-    description = description + " "+child.text    
-    description = description + ex2.pop()
-    
+    description = description + " "+child.text.strip()   
+    description = description + " "+ex2.pop().strip()
 description = description+" \n#nasa #space #esa #apod #astronomy #astrophotography"
-InstagramAPI = InstagramAPI("username", "password")
-InstagramAPI.login()  # login
-
-photo_path = './apod.jpg'
-InstagramAPI.uploadPhoto(photo_path, caption=description)
+print(description)
+try:
+    InstagramAPI = InstagramAPI("username", "password")
+    InstagramAPI.login()  # login
+    photo_path = './apod.jpg'
+    InstagramAPI.uploadPhoto(photo_path, caption=description)
+except:
+    print("Could not post to Instagram")
