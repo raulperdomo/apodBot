d2413575bcb42bba04b5711b62cf7abc5766a177
Raul Perdomo <raul@localhost.localdomain>
2018-09-28 01:06:08 -0400

    Reworked the ddescription parsing so that it was cleaner when posted.

--- instapod.py
+++ instapod.py
@@ -13,7 +13,7 @@ image = et.xpath('/html/body/center[1]/p[2]/a')
 imagePath = image.pop().attrib['href']
 apodPath = "http://apod.nasa.gov/"+imagePath
 print(apodPath)
-hiRes = urllib.request.urlretrieve(apodPath,'./apod.jpg')
+urllib.request.urlretrieve(apodPath,'./apod.jpg')
 img = Image.open('./apod.jpg')
 if img.size[0] > img.size[1]:
     longer_side = max(img.size)
@@ -31,24 +31,15 @@ if img.size[0] > img.size[1]:
     
 else:
     img.save("./apodPadded.jpg")
-explanation = et.xpath('/html/body/p[1]')
-ex2 = et.xpath('/html/body/p[1]/text()')
-ex2.reverse()
-description = ''
-for child in explanation[0].iter():
-    description = description + " "+child.text.strip()   
-    description = description + " "+ex2.pop().strip()
-description = description + '\n #nasa #space #esa #apod #astronomy #astrophotography\n'
-credit = et.xpath('/html/body/center[2]/b[2]')
-creditString = '📷: '
-creditString = creditString + credit[0].tail.strip() + " "
-credit = et.xpath('/html/body/center[2]/a')
-title = et.xpath('/html/body/center[2]/b[1]')[0].text.strip() + '\n'
-for text in credit:
-    creditString = creditString + text.text.strip()
-    creditString = creditString + " " +text.tail.strip()
 
-description = title + description + " " + creditString
+explanation = et.xpath('/html/body/p[1]')[0]
+description = ' '.join(explanation.text_content().split())
+description = description + '\n#nasa #space #esa #apod #astronomy #astrophotography\n'
+credit = et.xpath('/html/body/center[2]/a')[0].text_content()
+creditString = '📷: '+ ' '.join(credit.split())
+title = et.xpath('/html/body/center[2]/b[1]')[0].text.strip() + '\n'
+description = title + description + creditString
+print(description)
 try:
 	InstagramAPI = InstagramAPI("username", "password")
 	InstagramAPI.login()  # login
